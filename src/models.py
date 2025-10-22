cdf012d70d5b3957c445f08496b4645a38a65b38
Eossandon92 <81490666+Eossandon92@users.noreply.github.com>
2025-10-22 13:38:03 +0000

    final

--- src/models.py
+++ src/models.py
@@ -1,19 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Column, Table, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+followers = Table(
+    "followers",
+    db.metadata,
+    Column("user_from_id", ForeignKey("user.id")),
+    Column("user_to_id", ForeignKey("user.id"))
+)
+
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    username: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    fistname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
         }
+
+    following: Mapped[list["User"]] = relationship(
+        secondary=followers,
+        primaryjoin=id == followers.c.user_from_id,
+        secondaryjoin=id == followers.c.user_to_id,
+        backref="followers"
+    )
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[int]
+    url: Mapped[str] = mapped_column(String(500), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(1000))
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
